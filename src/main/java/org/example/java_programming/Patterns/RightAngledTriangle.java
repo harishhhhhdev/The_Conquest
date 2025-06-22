a4bee18f85311c90ec37b3b01e22bbb21e427ffe
Harish <harishanandan100@gmail.com>
2025-06-22 17:59:05 +0530

    Right Angled Triangle

--- src/main/java/org/example/java_programming/Patterns/RightAngledTriangle.java
+++ src/main/java/org/example/java_programming/Patterns/RightAngledTriangle.java
@@ -2,20 +2,17 @@ package org.example.java_programming.Patterns;
 
 public class RightAngledTriangle {
     public static void main(String[] args) {
-        int rows = 5; // Namma ethana row venum-nu kudukurathu
+        int rows = 5;
 
         // Outer loop → rows
         for (int i = 1; i <= rows; i++) {
-            // Inner loop → ithu row-ku ethana star print pannanum-nu
+            // Inner loop
             for (int j = 1; j <= i; j++) {
-                System.out.print("* "); // Star print pannuvom
+                System.out.print("* "); // Star print
             }
-            System.out.println(); // Oru row mudinja, next line poganum
+            System.out.println(); //
         }
     }
 }
 
 
-//git add
-//git commit -m "✅ Q1: Right-Angled Triangle pattern with Tanglish explanation"
-//git push
