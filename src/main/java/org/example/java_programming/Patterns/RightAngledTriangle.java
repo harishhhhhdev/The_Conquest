c1b3ab085925fe3363ad6549a321ddf35015cf09
Harish <harishanandan100@gmail.com>
2025-06-23 11:18:56 +0530

    Right Angled Triangle pattern

--- src/main/java/org/example/java_programming/Patterns/RightAngledTriangle.java
+++ src/main/java/org/example/java_programming/Patterns/RightAngledTriangle.java
@@ -2,18 +2,17 @@ package org.example.java_programming.Patterns;
 
 public class RightAngledTriangle {
     public static void main(String[] args) {
-        int rows = 5;
+       int rows = 5;
 
-        // Outer loop → rows
-        for (int i = 1; i <= rows; i++) {
-            // Inner loop
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* "); // Star print
-            }
-            System.out.println(); //
+       for (int i = 1; i <= rows; i++) {
+           for (int j = 1; j<= i; j++){
+               System.out.print("*  ");
+           }
+           System.out.println();
+       }
+
         }
     }
-}
 
 
 //git add
